d053913fbd22b8755469df0799245d248e81945b
skelkirk <nikolas217@mail.ru>
2020-06-09 20:24:34 +0300

    f

--- LessonOne/src/Lesson2/Task7.java
+++ LessonOne/src/Lesson2/Task7.java
@@ -12,24 +12,21 @@ import java.util.Arrays;
 public class Task7 {
 
     public static void main(String[] args) {
-        int[] arr={1,2,3,5,6,31,6,7};
+        int[] arr={1,2,3,4,5,6,7,8};
         int n=8;
+
         shifting(arr,n);
     }
     static void shifting(int[] array, int n){
-        int temp1=array[n-1];
-        int temp2=array[n-2];
-        int temp3=array[n-3];
-        for (int i = n-1; i >2 ; i--) {
-            array[i]=array[i-3];
-        }
-        array[2]=temp1;
-        array[1]=temp2;
-        array[0]=temp3;
-        System.out.println(Arrays.toString(array));
+
+        int shift=1;
+        int temp=0;
 
 
+
+
+            System.out.println(Arrays.toString(array));
     }
 
-    }
+}
 
